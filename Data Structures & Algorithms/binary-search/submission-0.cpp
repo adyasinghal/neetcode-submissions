4b04df86fea42c74dac4268a5009806a9e1281aa
Adya Singhal <adya.singhal@gmail.com>
2026-08-24 12:32:00 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int L = 0; 
+        int R = nums.size()-1;
+        int mid;
+        while (L <= R){
+            mid = L + (R-L)/2;
+            if (nums[mid] == target){ return mid; }
+            else if(nums[mid]<target){L = mid + 1;}
+            else{ R = mid -1; }
+        }
+        return -1;
+
+        
+    }
+};
